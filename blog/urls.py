72b80e50f585f428435943bad999c3313c9fb1f9
Aziza Zhalgaspayeva <duisembay12@gmail.com>
2022-09-30 17:31:50 +0600

    First commit!

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import (
+    all_blogs,
+    detail
+)
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', detail, name='detail'),
+]
